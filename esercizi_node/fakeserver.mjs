89bb80766431cb731c947aa5606f4fcd9a4960d0
Matt3ol <148490319+Matt3ol@users.noreply.github.com>
2024-03-28 12:50:59 +0100

    si

--- esercizi_node/fakeserver.mjs
+++
@@ -1,9 +0,0 @@
-import { createServer } from "node:http";
-
-
-const server = createServer((req, res) => {
- console.log(`Method: ${req.method}, URL:${req.url}`)
-
-server.listen(3000, () => {
-  console.log("Server listening http://localhost:3000");
-});
