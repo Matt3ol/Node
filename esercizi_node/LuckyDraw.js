3441acc5ed3fc04903c745f1fa50b47eed608792
Matt3ol <148490319+Matt3ol@users.noreply.github.com>
2024-03-29 15:35:52 +0100

    si

--- esercizi_node/LuckyDraw.js
+++ esercizi_node/LuckyDraw.js
@@ -1,28 +1,29 @@
 function luckyDraw(player) {
-  return new Promise((resolve, reject) => {
-    const win = Boolean(Math.round(Math.random()));
-
-    process.nextTick(() => {
-      if (win) {
-        resolve(`${player} won a prize in the draw!`);
-      } else {
-        reject(new Error(`${player} lost the draw.`));
-      }
+    return new Promise((resolve, reject) => {
+      const win = Boolean(Math.round(Math.random()));
+  
+      process.nextTick(() => {
+        if (win) {
+          resolve(`${player} won a prize in the draw!`);
+        } else {
+          reject(new Error(`${player} lost the draw.`));
+        }
+      });
     });
-  });
-}
-
-const players = ["Joe", "Caroline", "Sabrina"];
-
-const runDraw = async () => {
-  for (const player of players) {
-    try {
-      const result = await luckyDraw(player);
-      console.log(result);
-    } catch (error) {
-      console.error(error.message);
-    }
   }
-};
-
-runDraw();
+  
+  const players = ['Joe', 'Caroline', 'Sabrina'];
+  
+  const drawPromises = players.map(player =>
+    luckyDraw(player)
+      .then(result => {
+        console.log(result);
+      })
+      .catch(error => {
+        console.error(error.message);
+      })
+  );
+  
+  Promise.all(drawPromises).then(() => {
+    console.log('Draws completed for all players.');
+  });
